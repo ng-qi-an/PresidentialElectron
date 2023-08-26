b5957de373d68c296f6e09483974cd8f632ee584
Ethan C <ethan.chow.yi.xiang@sji.edu.sg>
2023-08-26 10:46:32 +0800

    Tharman View :)

---
+++ Presidential Elections/TharmanView.swift
@@ -0,0 +1,53 @@
+import SwiftUI
+
+struct TharmanView: View {
+    @State private var lightness = 0.0
+    var body: some View {
+        ZStack {
+            Color(.lightGray)
+                .opacity(0.3)
+                .ignoresSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Text("Namraht:")
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                    Spacer()
+                }
+                .padding(50)
+                
+                
+                ZStack {
+                    Rectangle()
+                        .frame(width: 240, height: 299)
+                        .foregroundColor(.white)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.black, lineWidth: 2)
+                        )
+                    
+                    Image("BaldMan")
+                        .scaledToFit()
+                    Image("BigForehead")
+                        .resizable()
+                        .frame(width: 140, height: 40)
+                        .offset(y: -51)
+                        .offset(x: -10)
+                        .opacity(lightness)
+                }
+                Text("Slide to polish head")
+                    .padding()
+                    .font(.title2)
+                Slider (value: $lightness, in: 0...1)
+                Text("Opacity of polish: \(lightness)")
+            }
+        }
+    }
+}
+
+struct TharmanView_Previews: PreviewProvider {
+    static var previews: some View {
+        TharmanView()
+    }
+}
