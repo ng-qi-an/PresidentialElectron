27a184a06e1f24b865f7ca7670541f664d77db1a
YJ Soon <yjsoon@tinkertanker.com>
2023-08-26 09:21:28 +0800

    Initial Commit

---
+++ Presidential Elections/Presidential_ElectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Presidential_ElectionsApp.swift
+//  Presidential Elections
+//
+//  Created by YJ Soon on 26/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Presidential_ElectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
